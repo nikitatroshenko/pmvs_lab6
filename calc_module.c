32a1b46293c9fb564e748d0e5e049a6ed05b2599
nikitatroshenko <nikitatroshenko@gmail.com>
2016-12-06 18:03:19 +0300

    Removed redundant declaration of init&cleanup module as they defined as __init and __exit corresponsively

--- calc_module.c
+++ calc_module.c
@@ -16,9 +16,6 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR(DRIVER_AUTHOR);
 MODULE_DESCRIPTION(DRIVER_DESC);
 
-int init_module(void);
-void cleanup_module(void);
-
 static ssize_t proc_read(struct file *, char *, size_t, loff_t *);
 
 static ssize_t dev_left_write(struct file *, const char *, size_t, loff_t *);
